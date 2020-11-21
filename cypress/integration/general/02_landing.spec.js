635edbdd9ed275e90e2464c1d89132055097a370
Yunlong Huang <yunlong.huang@polystar.com>
2020-11-21 20:19:34 +0100

    fix landing spec

--- cypress/integration/general/02_landing.spec.js
+++ cypress/integration/general/02_landing.spec.js
@@ -10,9 +10,9 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 		cy.clearCookie('hiqCookie')
 	})
 
-	it('Verify header section', function () {
+	it('Verify header sections', function () {
 		// Verify HIQ logo visible
-		cy.get('.header-bg-left').scrollIntoView({ force: true }).should('be.visible')
+		cy.get('.header-bg-left > img').scrollIntoView({ force: true }).should('be.visible')
 
 		// Verify language button visible
 		cy.get('button#trigger-langoverlay').should('be.visible')
@@ -22,20 +22,9 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 
 		// Verify meny visible
 		cy.get('.hamburger-box').should('be.visible')
-
-		// Verify page scroller visible
-		cy.get('.scroll-msg-wheel').should('be.visible')
-
-		// Verify hero text & link visible
-		cy.get('.hero-text > .font-hiq-rough').should('be.visible')
-		cy.get('.hero-text > .small-12').should('be.visible')
-		cy.get('.overflow-hidden > div > .hero-text > .link-arrow > a').should('be.visible')
-			.should('have.attr', 'href', '/case/').click({ force: true })
-		cy.url().should('include', '/case/').end()
-		cy.go('back')
 	})
 
-	it('Verify header language switch', function () {
+	it('Verify header language options', function () {
 		cy.get('button#trigger-langoverlay').scrollIntoView({ force: true }).should("be.visible")
 
 		var items = Number(Cypress.env('landing-language-options'))
@@ -49,8 +38,6 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 				.find('.lang > a')
 				.should("be.visible")
 				.should('have.attr', 'href', Cypress.env('landing-language-option-' + i))
-				.click()
-			cy.url().should('eq', Cypress.env('landing-language-option-' + i))
 		}
 	})
 
@@ -60,9 +47,9 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 		cy.get('.overlaylang').should('not.visible')
 	})
 
-	const search_keyword = ['development', 'frontend', 'devops', 'cloud', 'testautomatiserare']
+	const search_keyword = ['autonomous', 'frontend', 'devops', 'cloud', 'smart']
 	search_keyword.forEach((keyword) => {
-		it('C2429 Verify page search with keyword ' + keyword, function () {
+		it('Verify page search with keyword ' + keyword, function () {
 			cy.get('button#trigger-searchoverlay').scrollIntoView({ force: true }).click()
 			cy.get('input#query-layer').type(keyword)
 			cy.get('.query-button').click()
@@ -94,7 +81,6 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 			cy.url().should('include', Cypress.env('hamburger-menu-item-' + i))
 			cy.go('back')
 		}
-
 	})
 
 	it('Verify hamburger menu page close button', function () {
@@ -103,52 +89,213 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 		cy.get('.overlaymenu').should('not.visible')
 	})
 
-	it('Verify main culture section', function () {
-		// Verify culture talk  
-		cy.get('main#main-content > div:eq(0)').scrollIntoView({ force: true }).should('be.visible')
+	it('Verify hello world block', function () {
+		cy.get('.promotion-block:eq(0)').scrollIntoView({ force: true }).should('be.visible')
 
-		cy.get('main#main-content > div:eq(0) > div > div:eq(0) > div > section').should('be.visible')
-		cy.get('main#main-content > div:eq(0) > div > div:eq(0) > div > section > .orbit > .orbit-next').click()
-		cy.get('main#main-content > div:eq(0) > div > div:eq(0) > div > section > .orbit > .orbit-next').click()
-		cy.get('main#main-content > div:eq(0) > div > div:eq(0) > div > section > .orbit > .orbit-previous').click()
-		cy.get('main#main-content > div:eq(0) > div > div:eq(0) > div > section > .orbit > .orbit-bullets-wrapper > nav > button:eq(1)')
-			.should('have.attr', 'class', 'is-active')
-		cy.get('main#main-content > div:eq(0) > div > div:eq(1) > div > a').should('have.attr', 'href', '/jobb/').click()
-		cy.url().should('include', '/jobb/').end()
-		cy.go('back')
+		// Verify hello world block header and description
+		cy.get('.promotion-block:eq(0) > div:eq(0) > h1').should('be.visible').should('contain.text', 'Hello world!')
+		cy.get('.promotion-block:eq(0) > div:eq(1) > div').should('be.visible').should('contain.text', 'Välkommen till HiQ')
+
+		// Verify hello world block buttons' url
+		var items = Number(Cypress.env('promotion-block-buttons'))
+		var i
+		cy.get('.promotion-block:eq(0) > ul > li').should('have.length', Number(Cypress.env('hello-world-buttons')))
+		for (i = 0; i < items; i++) {
+			
+			cy.get('.promotion-block:eq(0) > ul > li')
+				.eq(i)
+				.find('a')
+				.should("be.visible")
+				.should('have.attr', 'href', Cypress.env('hello-world-button-' + i))
+		}		
 	})
 
-	it('Verify main case section', function () {
-		// Verify three cases
-		cy.get('main#main-content > div:eq(1)').scrollIntoView({ force: true }).should('be.visible')
+	it('Verify promotion questions block', function () {		
+		cy.get('.background-paper').scrollIntoView({ force: true }).should('be.visible')
 
-		var items = Number(Cypress.env('main-case-items'))
+		// Verify promotion questions
+		var items = Number(Cypress.env('promotion-questions'))
 		var i
-		cy.get('main#main-content > div:eq(1) > div').should('have.length', Number(Cypress.env('main-case-items')))
+		cy.get('.background-paper > div > div').should('have.length', Number(Cypress.env('promotion-questions'))+1)
+
+		for (i = 1; i <= items; i++) {
+			
+			cy.get('.background-paper > div > div')
+			    .eq(i-1)
+				.find('div:eq(0) > p')
+				.should('have.text', Cypress.env('promotion-question-' + i + '-no'))
+			cy.get('.background-paper > div > div')
+			    .eq(i-1)
+				.find('div:eq(1) > h2')
+				.should('contain.text', Cypress.env('promotion-question-' + i + '-heading'))
+		}
+
+		// Verify promotion question reply
+		cy.get('.background-paper > div > div')
+			.eq(items).find('div > p').should('contain.text', Cypress.env('promotion-questions-reply-part-0'))
+		cy.get('.background-paper > div > div')
+			.eq(items).find('div > p').should('contain.text', Cypress.env('promotion-questions-reply-part-1'))
+		
+		// Verify curiosity button
+		cy.get('.background-paper > ul > li > a')
+		    .should("be.visible")
+		    .should('have.attr', 'href', Cypress.env('curiosity-button'))
+	})
+
+	it('Verify join hiq block', function () {
+		cy.get('.promotion-block:eq(2)').scrollIntoView({ force: true }).should('be.visible')
+
+		// Verify join hiq block header and description
+		cy.get('.promotion-block:eq(2) > div:eq(0) > h2').should('be.visible').should('contain.text', 'Join HiQ!')
+		cy.get('.promotion-block:eq(2) > div:eq(1) > div')
+		    .should('be.visible').should('contain.text', 'Vi letar hela tiden efter smarta, kreativa och engagerade människor för anställning eller som underkonsulter via Tech Network.')
+
+		// Verify join hiq block buttons' url
+		var items = Number(Cypress.env('join-hiq-buttons'))
+		var i
+		cy.get('.promotion-block:eq(2) > ul > li').should('have.length', Number(Cypress.env('join-hiq-buttons')))
 		for (i = 0; i < items; i++) {
-			cy.get('main#main-content > div:eq(1) > div')
-				.eq(i).scrollIntoView({ force: true })
-				.find('> div > div > a')
+			
+			cy.get('.promotion-block:eq(2) > ul > li')
+				.eq(i)
+				.find('a')
 				.should("be.visible")
-				.should('have.attr', 'href', Cypress.env('main-case-item-' + i))
-				.click()
-			cy.url().should('include', Cypress.env('main-case-item-' + i))
-			cy.go('back')
+				.should('have.attr', 'href', Cypress.env('join-hiq-button-' + i))
 		}
 	})
 
-	it('Verify main news section', function () {
-		// Verify news section
-		cy.get('main#main-content > div:eq(2)').scrollIntoView({ force: true }).should('be.visible')
+	it('Verify hiq digital knowledge tour block', function () {
+		cy.get('.promotion-block:eq(3)').scrollIntoView({ force: true }).should('be.visible')
 
-		cy.get('main#main-content > div:eq(2) > div > div:eq(0) > a').first().should('be.visible').click()
+		// Verify hiq digital knowledge tour block header and description
+		cy.get('.promotion-block:eq(3) > div:eq(0) > h2').should('be.visible').should('contain.text', 'HiQ Digital Knowledge Tour')
+		cy.get('.promotion-block:eq(3) > div:eq(1) > div')
+		    .should('be.visible').should('contain.text', 'Vi bjuder på matnyttiga och kostnadsfria webbinarier om tech, IT, design och integration.')
+
+		// Verify hiq digital knowledge tour block button' url
+		cy.get('.promotion-block:eq(3) > ul > li > a')
+		    .should("be.visible")
+			.should('have.attr', 'href', '/ideer/hiq-digital-knowledge-tour/')	
+
+	})
+
+	it('Verify meet the family block', function () {
+		cy.get('.promotion-block:eq(4)').scrollIntoView({ force: true }).should('be.visible')
+
+		// Verify meet the family block header and description
+		cy.get('.promotion-block:eq(4) > div:eq(0) > h2').should('be.visible').should('contain.text', 'Meet the family')
+
+		// Verify meet the family block buttons' url
+		var items = Number(Cypress.env('meet-the-family-buttons'))
+		var i
+		cy.get('.promotion-block:eq(4) > ul > li').should('have.length', Number(Cypress.env('meet-the-family-buttons')))
+		for (i = 0; i < items; i++) {		
+			cy.get('.promotion-block:eq(4) > ul > li')
+				.eq(i)
+				.find('a')
+				.should("be.visible")
+				.should('have.attr', 'href', Cypress.env('meet-the-family-button-' + i))
+		}
+	})
+
+	it('Verify news block', function () {
+		cy.get('.news-list:eq(0)').scrollIntoView({ force: true }).should('be.visible')
+
+		// Verify news block
+		cy.get('.news-list:eq(0) > div:eq(0) > h3').should('be.visible').should('have.text', 'Nyheter')
+
+		// Verify meet the family block buttons' url
+		var items = Number(Cypress.env('news-list-items'))
+		var i
+		cy.get('.news-list:eq(1) > div > a').should('have.length', Number(Cypress.env('news-list-items')))
+		for (i = 0; i < items; i++) {
+			
+			cy.get('.news-list:eq(1) > div > a')
+				.eq(i)
+				.find('article')
+				.should("be.visible")
+		}
+
+		cy.get('.news-show-all').should('be.visible')
+			.should('have.attr', 'href', '/nyheter/').should('have.text', 'Alla nyheter').click()
 		cy.url().should('include', '/nyheter/')
-		cy.go('back')
-		cy.get('main#main-content > div:eq(2) > div > div:eq(1) > div > a').scrollIntoView({ force: true })
-			.should('be.visible')
-			.should('have.attr', 'href', '/nyheter/').click()
-		cy.url().should('include', '/nyheter/').end()
-		cy.go('back')
+		cy.go('back')		
+	})
+
+	it('Verify footer sections', function () {
+		cy.get('footer#main-footer > div').scrollIntoView({ force: true }).should('be.visible')
+
+		// Verify footer logo
+		cy.get('.footer-logo').should('be.visible')
+
+		// Verify footer quote
+		cy.get('.footer-quote').should('be.visible')
+		cy.get('.footer-quote > h4:eq(0)').should('be.visible').should('contain.text', 'Our dealings are complex, our duty simplicity')
+		cy.get('.footer-quote > h4:eq(1)').should('be.visible').should('contain.text', 'Bengan')
+
+		// Verify footer list
+		var items = Number(Cypress.env('footer-list-items'))
+		var i
+		cy.get('.footer-list > li')
+			.should('have.length', Number(Cypress.env('footer-list-items')))
+		for (i = 0; i < items; i++) {
+			if (i == 0) {
+				cy.get('.footer-list > li')
+				    .eq(i)
+				    .find('ul > li:eq(0) > h5')
+				    .should("be.visible")
+					.should('contain.text', Cypress.env('footer-list-item-' + i))
+			} else {
+				cy.get('.footer-list > li')
+				    .eq(i)
+				    .find('h5')
+				    .should("be.visible")
+				    .should('contain.text', Cypress.env('footer-list-item-' + i))
+			}	
+		}
+	})
+
+	it('Verify footer contact hiq', function () {
+		cy.get('footer#main-footer > div').scrollIntoView({ force: true }).should('be.visible')
+
+		var items = Number(Cypress.env('footer-contact-hiq-items'))
+		var i
+		cy.get('.footer-list > li:eq(0) > ul > li')
+			.should('have.length', Number(Cypress.env('footer-contact-hiq-items')) + 1)
+		for (i = 1; i < items + 1; i++) {
+			cy.get('.footer-list > li:eq(0) > ul > li')
+				.eq(i)
+				.find('small')
+				.should("be.visible")
+				.should('contain.text', Cypress.env('footer-contact-hiq-item-' + (i-1) + '-text'))
+			cy.get('.footer-list > li:eq(0) > ul > li')
+				.eq(i)
+				.find('small > a')
+				.should("be.visible")
+				.should('have.attr', 'href', Cypress.env('footer-contact-hiq-item-' + (i-1) + '-mail'))
+		}
+	})
+
+	it('Verify footer links', function () {
+		cy.get('footer#main-footer > div').scrollIntoView({ force: true }).should('be.visible')
+
+		var items = Number(Cypress.env('footer-link-items'))
+		var i
+		cy.get('.footer-list > li:eq(1) > small > ul > li')
+		    .should('have.length', Number(Cypress.env('footer-link-items')))
+		for (i = 0; i < items; i++) {
+			if (i != 2 && i != 4) {
+				cy.get('.footer-list > li:eq(1) > small > ul > li')
+					.eq(i)
+					.find('a')
+					.should("be.visible")
+					.should('have.text', Cypress.env('footer-link-item-' + i))
+					.should('have.attr', 'href', Cypress.env('footer-link-item-' + i + '-url'))
+					.click()
+				cy.url().should('include', Cypress.env('footer-link-item-' + i + '-url'))
+				cy.go('back')
+			}
+		}
 	})
 
 	it('Verify footer social media', function () {
@@ -156,55 +303,57 @@ describe(Cypress.env('brand').toUpperCase() + ' - landing page', function () {
 
 		var items = Number(Cypress.env('footer-social-media-items'))
 		var i
-		cy.get('footer#main-footer > div > div > div > div:eq(0) > div')
-			.should('have.length', Number(Cypress.env('footer-social-media-items')) + 1)
+		cy.get('.footer-list > li:eq(2) > ul > li')
+			.should('have.length', Number(Cypress.env('footer-social-media-items')))
 		for (i = 0; i < items; i++) {
-			if (i != 2) {
-				cy.get('footer#main-footer > div > div > div > div:eq(0) > div')
-					.eq(i)
-					.find('a')
-					.should("be.visible")
-					.should('have.attr', 'href', Cypress.env('footer-social-media-item-' + i))
-			}
+			cy.get('.footer-list > li:eq(2) > ul > li')
+				.eq(i)
+				.find('a')
+				.should("be.visible")
+				.should('have.attr', 'href', Cypress.env('footer-social-media-item-' + i + '-url'))
+			cy.get('.footer-list > li:eq(2) > ul > li')
+				.eq(i)
+				.find('a > small')
+				.should("be.visible")
+				.should('have.text', Cypress.env('footer-social-media-item-' + i))
 		}
-
 	})
 
-	it('Verify footer links', function () {
-		cy.get('footer#main-footer > div').scrollIntoView({ force: true })
+	it('Verify footer offices', function () {
+		cy.get('footer#main-footer > div').scrollIntoView({ force: true }).should('be.visible')
 
-		var items = Number(Cypress.env('footer-link-items'))
+		var items = Number(Cypress.env('footer-offices'))
 		var i
-		cy.get('footer#main-footer > div > div > div > div:eq(1) > ul > li').should('have.length', Number(Cypress.env('footer-link-items')))
+		cy.get('.footer-list > li:eq(3) > small > ul > li')
+			.should('have.length', Number(Cypress.env('footer-offices')))
 		for (i = 0; i < items; i++) {
-			if (i != 3) {
-				cy.get('footer#main-footer > div').scrollIntoView({ force: true })
-				cy.get('footer#main-footer > div > div > div > div:eq(1) > ul > li')
-					.eq(i)
-					.find('a')
-					.should("be.visible")
-					.should('have.attr', 'href', Cypress.env('footer-link-item-' + i))
-					.click()
-				cy.url().should('include', Cypress.env('footer-link-item-' + i))
-				cy.go('back')
-			}
+			cy.get('.footer-list > li:eq(3) > small > ul > li')
+				.eq(i)
+				.find('a')
+				.should("be.visible")
+				.should('have.text', Cypress.env('footer-office-' + i))
+				.should('have.attr', 'href', Cypress.env('footer-office-' + i + '-job-url'))
+	
+		}
+
+	})
+
+	it('Verify footer hiq familjens', function () {
+		cy.get('footer#main-footer > div').scrollIntoView({ force: true }).should('be.visible')
+
+		var items = Number(Cypress.env('footer-hiq-familjens'))
+		var i
+		cy.get('.footer-list > li:eq(4) > small > ul > li')
+			.should('have.length', Number(Cypress.env('footer-hiq-familjens')))
+		for (i = 0; i < items; i++) {
+			cy.get('.footer-list > li:eq(4) > small > ul > li')
+				.eq(i)
+				.find('a')
+				.should("be.visible")
+				.should('have.text', Cypress.env('footer-hiq-familjen-' + i))
+				.should('have.attr', 'href', Cypress.env('footer-hiq-familjen-' + i + '-url'))
 		}
 	})
 
-	it('Verify footer projects', function () {
-		cy.get('.footer-projects-left').scrollIntoView({ force: true }).should('be.visible')
-		cy.get('.footer-projects-right > a').scrollIntoView({ force: true }).should('be.visible')
 
-	})
-
-	it('Verify open application form', function () {
-		cy.get('.footer-projects-right > a').scrollIntoView({ force: true }).should('be.visible').click()
-		cy.get('div#openApplicationFooter').should('be.visible')
-		cy.get('.Form__MainBody > section > div:eq(0) > input').type("Test Test")
-		cy.get('.Form__MainBody > section > div:eq(1) > input').type("testare@gmail.com")
-		cy.get('.Form__MainBody > section > div:eq(2) > input').type("070110120")
-		cy.get('.Form__MainBody > section > div:eq(3) > textarea').type("Åh vad fint!")
-		cy.get('.Form__MainBody > section > button').scrollIntoView({ force: true }).should('be.visible')
-		cy.get('div#openApplicationFooter > .close-button').scrollIntoView({ force: true }).click()
-	})
 })
