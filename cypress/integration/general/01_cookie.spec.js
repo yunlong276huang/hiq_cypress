d365f96b4777683b1abe23ccfacb695c083f410e
Yunlong Huang <yunlong.huang@polystar.com>
2022-01-09 14:53:38 +0100

    add dedicated context text info on one test

--- cypress/integration/general/01_cookie.spec.js
+++ cypress/integration/general/01_cookie.spec.js
@@ -10,6 +10,16 @@ describe(Cypress.env('brand').toUpperCase() + ' - cookie', function () {
         })
 
         it('Verify cookie banner', function () {
+            cy.addTestContext(
+                'Test Description',
+                'Test Steps: ' +
+                '\n1. Visit landing page for the first time' +
+                '\n2. Check cookie banner is visible' +
+                '\n3. Click OK button to approve cookie consent' +
+                '\n4. Verify cookie hiqCookie value is set to 1 and the cookie banner disappears' +
+                '\n\nTest Script last modified by: Yunlog Huang'
+            )
+
             // Verify cookie banner visible and accept it
             cy.get('div#cookie-info').scrollIntoView({ force: true }).should('be.visible')
             cy.get('div#cookie-info > div > a').click()
